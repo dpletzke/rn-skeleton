d61b235d4fa38e5c3c63d236f526059d7a83436a
dpletzke <dpletzke@gmail.com>
2023-09-26 23:36:33 -0500

    chore: update example environment file
    The example environment file (.example.env) was updated to include a comment with instructions on how to fill it out and update the secrets. This change was made to improve the usability of the file.
    
    feat: add app configuration file
    
    A new file (app.config.ts) was added to the project. This file contains the configuration for the Expo app, including the name, version, orientation, icons, splash screen, asset bundle patterns, iOS and Android specific configurations, web bundler settings, plugins, experiments, and extra settings. This change was made to provide a centralized location for app configuration and improve maintainability.
    
    refactor: remove obsolete app.json file
    
    The app.json file was removed from the project as it is no longer needed. The configuration settings from app.json were migrated to the new app.config.ts file. This change was made to simplify the project structure and remove redundant configuration files.
    
    chore: update Atlas configuration
    
    The appId in the atlasConfig.json file was updated to "breathe-easy-sync-db-bbiwm". This change was made to reflect the correct appId for the Atlas configuration.
    
    chore: update package name in package.json
    
    The name field in the package.json file was updated to "breathe-easy". This change was made to reflect the correct package name for the project.

---
+++ app.config.ts
@@ -0,0 +1,64 @@
+import { ExpoConfig } from "expo/config";
+
+export default (): ExpoConfig => {
+  return {
+    name: "breathe-easy",
+    slug: "breathe-easy-app",
+    version: "1.0.0",
+    orientation: "portrait",
+    icon: "./assets/images/icon.png",
+    scheme: "myapp",
+    userInterfaceStyle: "automatic",
+    splash: {
+      image: "./assets/images/splash.png",
+      resizeMode: "contain",
+      backgroundColor: "#ffffff",
+    },
+    assetBundlePatterns: ["**/*"],
+    ios: {
+      supportsTablet: true,
+      config: {
+        googleMapsApiKey: process.env.GOOGLE_MAPS_API_KEY,
+      },
+      bundleIdentifier: "com.dpletzke.breatheeasyapp",
+    },
+    android: {
+      adaptiveIcon: {
+        foregroundImage: "./assets/images/adaptive-icon.png",
+        backgroundColor: "#ffffff",
+      },
+      permissions: [
+        "android.permission.ACCESS_COARSE_LOCATION",
+        "android.permission.ACCESS_FINE_LOCATION",
+        "android.permission.FOREGROUND_SERVICE",
+      ],
+    },
+    web: {
+      bundler: "metro",
+      output: "static",
+      favicon: "./assets/images/favicon.png",
+    },
+    plugins: [
+      "expo-router",
+      [
+        "expo-location",
+        {
+          locationAlwaysAndWhenInUsePermission:
+            "Allow $(PRODUCT_NAME) to use your location.",
+        },
+      ],
+    ],
+    experiments: {
+      typedRoutes: true,
+    },
+    extra: {
+      router: {
+        origin: false,
+      },
+      eas: {
+        projectId: "53f14974-1125-471d-b38e-5b9a01d61f54",
+      },
+    },
+    owner: "dpletzke",
+  };
+};
