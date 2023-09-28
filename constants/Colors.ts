d57faffa6dae866ba0a3c7cbdd9b459269fc4d1f
dpletzke <dpletzke@gmail.com>
2023-09-28 01:41:57 -0500

    chore(Colors.ts): update tintColorLight and tintColorDark values to improve color scheme feat(Colors.ts): add ColorTiers type for better type safety and readability feat(Colors.ts): add primary, secondary, success, danger, warning, and info colors for both light and dark themes

--- constants/Colors.ts
+++ constants/Colors.ts
@@ -1,19 +1,39 @@
-const tintColorLight = '#2f95dc';
-const tintColorDark = '#fff';
+const tintColorLight = "#007AFF";
+const tintColorDark = "#0A84FF";
+
+export type ColorTiers =
+  | "success"
+  | "danger"
+  | "warning"
+  | "info"
+  | "primary"
+  | "secondary";
 
 export default {
   light: {
-    text: '#000',
-    background: '#fff',
+    text: "#000",
+    background: "#F5F5F5",
     tint: tintColorLight,
-    tabIconDefault: '#ccc',
+    tabIconDefault: "#8E8E93",
     tabIconSelected: tintColorLight,
+    primary: "#007AFF",
+    secondary: "#34C759",
+    success: "#28A745",
+    danger: "#DC3545",
+    warning: "#FF9500",
+    info: "#17A2B8",
   },
   dark: {
-    text: '#fff',
-    background: '#000',
+    text: "#fff",
+    background: "#121212",
     tint: tintColorDark,
-    tabIconDefault: '#ccc',
+    tabIconDefault: "#8E8E93",
     tabIconSelected: tintColorDark,
+    primary: "#0A84FF",
+    secondary: "#30D158",
+    success: "#32D74B",
+    danger: "#FF453A",
+    warning: "#FF9F0A",
+    info: "#64D2FF",
   },
 };
