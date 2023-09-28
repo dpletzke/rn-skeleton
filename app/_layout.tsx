c1fad31266dfd6dc76ffbf0ab7517ee72e798e87
dpletzke <dpletzke@gmail.com>
2023-09-28 01:34:29 -0500

    refactor(_layout.tsx): remove unused imports and commented code to improve code cleanliness and readability feat(_layout.tsx): add AppWrapper component to wrap the app and provide necessary context and navigation stack feat(_layout.tsx): add new screens and options to the navigation stack for better app navigation flow feat(_layout.tsx): add Inter font file to assets/fonts directory for use in the app

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,18 +1,8 @@
 import { useEffect } from "react";
-import { ActivityIndicator, View, useColorScheme } from "react-native";
 import FontAwesome from "@expo/vector-icons/FontAwesome";
-import {
-  DarkTheme,
-  DefaultTheme,
-  ThemeProvider,
-} from "@react-navigation/native";
 import { useFonts } from "expo-font";
 import { SplashScreen, Stack } from "expo-router";
-import { useNotifications } from "../hooks";
-import { AppProvider, UserProvider } from "@realm/react";
-import { RealmProvider } from "../schemas";
-import { appId, baseUrl } from "../atlasConfig.json";
-import Login from "../components/Login";
+import { AppWrapper } from "../components/AppWrapper";
 
 export {
   // Catch any errors thrown by the Layout component.
@@ -21,28 +11,27 @@ export {
 
 export const unstable_settings = {
   // Ensure that reloading on `/modal` keeps a back button present.
-  initialRouteName: "(tabs)",
+  initialRouteName: "index",
 };
 
 // Prevent the splash screen from auto-hiding before asset loading is complete.
 SplashScreen.preventAutoHideAsync();
 
 export default function RootLayout() {
+  // const [loaded, error] = useFonts({
+  //   SpaceMono: require("../assets/fonts/SpaceMono-Regular.ttf"),
+  //   ...FontAwesome.font,
+  // });
+
   const [loaded, error] = useFonts({
-    SpaceMono: require("../assets/fonts/SpaceMono-Regular.ttf"),
+    Inter: require("../assets/fonts/Inter-VariableFont_slnt,wght.ttf"),
     ...FontAwesome.font,
   });
-
-  useEffect(() => {
-    console.log("RootLayout loaded");
-  }, []);
 
   // Expo Router uses Error Boundaries to catch errors in the navigation tree.
   useEffect(() => {
     if (error) throw error;
   }, [error]);
-
-  const [expoPushToken] = useNotifications();
 
   useEffect(() => {
     if (loaded) {
@@ -54,48 +43,18 @@ export default function RootLayout() {
     return null;
   }
 
-  return <RootLayoutNav />;
-}
-
-const LoadingIndicator = () => {
   return (
-    <View>
-      <ActivityIndicator size="large" />
-    </View>
-  );
-};
-
-function RootLayoutNav() {
-  const colorScheme = useColorScheme();
-
-  return (
-    <AppProvider id={appId} baseUrl={baseUrl}>
-      <UserProvider fallback={Login}>
-        <RealmProvider
-          fallback={LoadingIndicator}
-          sync={{
-            flexible: true,
-            onError: (session, error) => {
-              console.log(error);
-            },
-            initialSubscriptions: {
-              update: (subs, realm) => {
-                subs.add(realm.objects("Task"));
-              },
-              rerunOnOpen: true,
-            },
-          }}
-        >
-          <ThemeProvider
-            value={colorScheme === "dark" ? DarkTheme : DefaultTheme}
-          >
-            <Stack>
-              <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-              <Stack.Screen name="modal" options={{ presentation: "modal" }} />
-            </Stack>
-          </ThemeProvider>
-        </RealmProvider>
-      </UserProvider>
-    </AppProvider>
+    <AppWrapper>
+      <Stack>
+        <Stack.Screen name="index" options={{ headerShown: false }} />
+        <Stack.Screen name="notifier/[notifierId]" />
+        <Stack.Screen
+          name="(createNotifier)"
+          options={{ headerShown: false }}
+        />
+        <Stack.Screen name="modal" options={{ presentation: "modal" }} />
+        {/* <Stack.Screen name="(tabs)" options={{ headerShown: false }} /> */}
+      </Stack>
+    </AppWrapper>
   );
 }
