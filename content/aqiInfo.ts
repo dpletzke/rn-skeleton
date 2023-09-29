2faf19376856895d48c99b06a7e6f6337c1aec38
dpletzke <dpletzke@gmail.com>
2023-09-28 19:41:03 -0500

    feat(aqiInfo.ts): add AQI levels array with corresponding values, colors, labels, and descriptions
    feat(index.ts): export AQI info module to be used in other parts of the application

---
+++ content/aqiInfo.ts
@@ -0,0 +1,44 @@
+export const aqiLevels = [
+  {
+    value: 50,
+    color: "#00e400",
+    label: "Good",
+    description:
+      "Air quality is considered satisfactory, and air pollution poses little or no risk.",
+  },
+  {
+    value: 100,
+    color: "#ffff00",
+    label: "Moderate",
+    description:
+      "Air quality is acceptable. However, there may be a risk for some people, particularly those who are unusually sensitive to air pollution.",
+  },
+  {
+    value: 150,
+    color: "#ff7e00",
+    label: "Unhealthy for Sensitive Groups",
+    description:
+      "Members of sensitive groups may experience health effects. The general public is less likely to be affected.",
+  },
+  {
+    value: 200,
+    color: "#ff0000",
+    label: "Unhealthy",
+    description:
+      "Some members of the general public may experience health effects; members of sensitive groups may experience more serious health effects.",
+  },
+  {
+    value: 300,
+    color: "#8f3f97",
+    label: "Very Unhealthy",
+    description:
+      "Health alert: The risk of health effects is increased for everyone.",
+  },
+  {
+    value: Infinity,
+    color: "#7e0023",
+    label: "Hazardous",
+    description:
+      "Health warning of emergency conditions: everyone is more likely to be affected.",
+  },
+];
